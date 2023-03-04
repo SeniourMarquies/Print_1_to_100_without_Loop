bbed9fef202f5b3ea22bad0279390fd7004117f4
SeniourMarquies <sap_al@hotmail.com>
2023-03-04 21:24:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+PrintToHundred(1);
+Console.WriteLine("------------------------------------------");
+PrintHundred(0, 100);
+
+
+void PrintToHundred(int n)
+{
+    if (n == 0)
+        return;
+    if (n < 0) return;
+    if(n <= 100)
+    {
+        Console.WriteLine(n);
+        n++;
+        PrintToHundred(n);
+    }
+}
+void PrintHundred(int startNum, int endNum)
+{
+   
+   
+    if (startNum <= endNum)
+    {
+        Console.WriteLine(startNum);
+        startNum++;
+        PrintHundred(startNum, endNum);
+    }
+}
